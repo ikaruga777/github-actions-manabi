e509e966a9dd6e95f3b05acb829ec723df8b0526
ikaruga <ikaruga777@gmail.com>
2020-03-14 13:58:28 +0900

    Add test of fizzbuzz

---
+++ test/index.js
@@ -0,0 +1,16 @@
+const assert = require("assert");
+const fizzbuzz = require("../index");
+
+describe("fizzbuzz", () => {
+  it("returns FizzBuzz when value is divisible by 15", () => {
+    assert(fizzbuzz(30) === "FizzBuzz");
+  });
+  
+  it("returns Fizz when value is divisble by 3", () => {
+    assert(fizzbuzz(6) === "Fizz");
+  });
+
+  it("returns Buzz when value is divisble by 5", () => {
+    assert(fizzbuzz(10) === "Buzz");
+  });
+});
